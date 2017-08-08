5f5eda0c9562532c126fd48975b14c85f7f803d6
R G <bhagyashree.r.g@accenture.com>
2017-08-08 18:22:11 +0530

    t1

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,4 +1,10 @@
 package hello;
+
 public class main {
- public static void HelloWorld(String[] args) {  // TODO Auto-generated method stub
-  System.out.println("HEllo world"); }}
+
+	public static void HelloWorld(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HEllo world");
+	}
+}
